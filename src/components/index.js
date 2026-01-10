62c616e746d461467ee6b70b293faff2151dc930
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-01-10 19:35:03 +0200

    add index.js to export core components for the blog application

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ProfileHeader } from "./ProfileHeader";
+export { default as FilterButtons } from "./FilterButtons";
+export { default as BlogCard } from "./BlogCard";
+export { default as BlogGrid } from "./BlogGrid";
+export { default as BlogDetailsPage } from "./BlogDetailsPage";
